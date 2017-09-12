764342b5a5235143ab454ad2b8c43230a1ba70dd
Chris Nady <nady.christopher@gmail.com>
2017-09-12 19:18:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,18 +10,18 @@ class Triangle
 
   def kind
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0
-      TriangleError
+      raise TriangleError
     elsif @side1 + @side2 < @side3 || @side1 +@side3 < @side2 || @side2 + @side3 < @side1
-      TriangleError
+      raise TriangleError
     end
-    
+
     if @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
     else
       :scalene
-    end      
+    end
   end
 
 
